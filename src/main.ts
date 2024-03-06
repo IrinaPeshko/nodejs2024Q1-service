6f8c9b3afb481b8678fb95f2c61e4e72d4c68c5f
IrinaPeshko <privet-kira1999@mail.ru>
2024-03-06 23:11:43 +0300

    feat: create swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { parse } from 'yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const docFile = await readFile(join(dirname(__dirname), 'doc', 'api.yaml'), 'utf-8');
+  const parsedDocFile = parse(docFile);
+
+  SwaggerModule.setup('doc', app, parsedDocFile);
   await app.listen(4000);
 }
 bootstrap();
