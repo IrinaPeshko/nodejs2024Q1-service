ffbcce2e8030cca01b689f6c5548f732ceb76b53
IrinaPeshko <privet-kira1999@mail.ru>
2024-03-17 19:04:54 +0300

    feat: create user in postgreSQL

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { db } from './services/db';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    const users = db.user.findMany();
     return 'Hello World!';
   }
 }
--- src/endpoints/user/user.service.ts
+++ src/endpoints/user/user.service.ts
@@ -7,97 +7,113 @@ import {
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
-import { v4 as uuidv4, validate as uuidValidate } from 'uuid';
+import { validate as uuidValidate } from 'uuid';
 import { db } from 'src/services/db';
 
 @Injectable()
 export class UserService {
   private users: User[] = [];
 
-  create(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto) {
     const { login, password } = createUserDto;
     if (!login || !password) {
       throw new BadRequestException(
         'Missing required fields. Please ensure all required fields are provided',
       );
     }
-    const newUser = new User();
-    newUser.id = uuidv4();
-    newUser.login = login;
-    newUser.password = password;
-    newUser.version = 1;
-    newUser.createdAt = Date.now();
-    newUser.updatedAt = Date.now();
-    this.users.push(newUser);
-    const response = { ...newUser };
-    delete response.password;
-    return response;
+    const newUser = await db.user.create({
+      data: {
+        login,
+        password,
+        version: 1,
+      },
+    });
+
+    const result = this.changeUserDate(newUser);
+    delete result.password;
+    return result;
   }
 
   async findAll() {
-    const users = await db.user.findMany();
-    // const users = this.users.map((user) => {
-    //   const response = { ...user };
-    //   delete response.password;
-    //   return response;
-    // });
-    return users;
+    const users = await db.user.findMany({
+      select: {
+        id: true,
+        login: true,
+        version: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+    const result = users.map((user) => {
+      return this.changeUserDate(user);
+    });
+    return result;
   }
 
-  findOne(id: string) {
+  async findOne(id: string) {
     if (!uuidValidate(id)) {
       throw new BadRequestException(
         'Invalid userId provided. Please provide a valid UUID.',
       );
     }
-    const user = this.users.find((user) => user.id === id);
+    const user = await db.user.findUnique({
+      where: { id },
+    });
     if (!user)
       throw new NotFoundException('User with the provided id does not exist.');
-    return user;
+    return this.changeUserDate(user);
   }
 
-  update(id: string, updateUserDto: UpdateUserDto) {
+  async update(id: string, updateUserDto: UpdateUserDto) {
     const { oldPassword, newPassword } = updateUserDto;
     if (!oldPassword || !newPassword) {
       throw new BadRequestException(
         'Invalid oldPassword or newPassword provided. Please provide a valid fields.',
       );
     }
-    const updatedUserIndex = this.findUserId(id);
-    const user = this.users[updatedUserIndex];
+    const user = await this.findUser(id);
     if (user.password !== oldPassword) {
       throw new ForbiddenException(
         'Old password is incorrect. Please provide the correct old password.',
       );
     }
 
-    const updatedUser: User = {
-      ...user,
-      password: updateUserDto.newPassword,
-      version: user.version + 1,
-      updatedAt: Date.now(),
-    };
-    this.users[updatedUserIndex] = updatedUser;
-    const response = { ...updatedUser };
-    delete response.password;
-    return response;
+    const updatedUser = await db.user.update({
+      where: { id },
+      data: {
+        password: newPassword,
+        version: { increment: 1 },
+        updatedAt: new Date(),
+      },
+    });
+    const result = this.changeUserDate(updatedUser);
+    delete result.password;
+    return result;
   }
 
-  remove(id: string) {
-    const userIndex = this.findUserId(id);
-    this.users.splice(userIndex, 1);
-    return `This action removes a #${id} user`;
+  async remove(id: string) {
+    await this.findUser(id);
+    await db.user.delete({ where: { id } });
+    return `User with id ${id} removed successfully.`;
   }
 
-  private findUserId(id: string) {
+  async findUser(id: string) {
     if (!uuidValidate(id)) {
       throw new BadRequestException(
         'Invalid userId provided. Please provide a valid UUID.',
       );
     }
-    const userIndex = this.users.findIndex((user) => user.id === id);
-    if (userIndex === -1)
+    const user = await db.user.findUnique({ where: { id } });
+    if (!user)
       throw new NotFoundException('User with the provided id does not exist.');
-    return userIndex;
+    return user;
   }
+
+  private changeUserDate = (user) => {
+    return {
+      ...user,
+      createdAt: user.createdAt.getTime(),
+      updatedAt: user.updatedAt.getTime(),
+    };
+  };
 }
